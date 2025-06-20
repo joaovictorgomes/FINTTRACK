d36aa0af660b7c09708cbc1b5e84ce2eb2eaa91e
Joao <joao.silva@togodynamics.com>
2025-06-20 11:27:09 -0300

    chore: setup Prisma Accelerate with Data Proxy and fix typing issues

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,14 @@
-// lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+  prisma?: ReturnType<PrismaClient['$extends']>
+}
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient().$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
 }
